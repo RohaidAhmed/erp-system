56bf91de0bcd2c67eb99e87112fb2794a7c7b701
rohaid.ahmed <rohaid.ahmed@bcubelimited.com>
2026-03-09 14:46:38 +0500

    some fixes

---
+++ lib/utils/utils.ts
@@ -0,0 +1,7 @@
+import { Department } from "@/types";
+
+export const department: Department[] = [
+    {id: "sw", name: "Software"},
+    {id: "hw", name: "Hardware"},
+    {id: "rf", name: "RF"},
+];
